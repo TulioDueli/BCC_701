62424528ccc979be853d160a980f37a17539ab83
Túlio Dueli <58995136+TulioDueli@users.noreply.github.com>
2023-03-08 00:20:26 -0300

    Update [AP-5.3] Questão 3.py

--- 5 Estrutura de Repetição Laços Aninhados/[AP-5.3] Questão 3.py
+++ 5 Estrutura de Repetição Laços Aninhados/[AP-5.3] Questão 3.py
@@ -5,8 +5,9 @@ x=0
 while c=='não' or c=='n' or c=='Não' or c=='nao':
     p1+=1
     p= int(input('\nQuantidade de itens do pedido: '))
+    pre=0
     for i in range(1, p+1):
-        pre= float(input(f'. Preço do item {i}: '))
+        pre+= float(input(f'. Preço do item {i}: '))
     d= input('. Cobrar delivery? ')
     if d=='sim':
         pre+=15
