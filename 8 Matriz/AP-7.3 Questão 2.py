5666a0df9c5fc9e4396bbe9e201cb060c24f3095
Túlio Dueli <58995136+TulioDueli@users.noreply.github.com>
2023-03-15 01:00:40 -0300

    Update AP-7.3 Questão 2.py

--- 8 Matriz/AP-7.3 Questão 2.py
+++ 8 Matriz/AP-7.3 Questão 2.py
@@ -1,11 +1,19 @@
 from biblioteca import*
+
 m= inputMatriz('Digite os elementos da matriz: ', int)
 qlin, qcol= dimMatriz(m)
+
 if qlin!=qcol:
     print('\nA matriz não é quadrada.')
+    
 else:
     print('\nElementos da diagonal principal:')
     for i in range(len(m)):
         print(f'{m[i][i]}', end='' )
         if i<len(m)-1:
             print(', ', end='')
+#   for i in range(len(m)-1):
+#       print(f'{m[i][i]},', end='')
+#   for i in range(len(m)-1, len(m)):
+#       print(f'{m[i][i]}')
+#   ^^^^^ Outra forma de fazer ^^^^^
